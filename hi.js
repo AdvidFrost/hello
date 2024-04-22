af732443899b52ea9169ba7eb13377702c4c1cc5
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-04-22 08:25:30 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,14 +1,10 @@
-/// urun.js
-document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//github.com/Inglan2/uRun/blob/main/popup.html"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});
-	}
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+      myCustomFunction();
+  }
 });
+
+function myCustomFunction() {
+  
+  console.log('Key combination pressed.');
+}
