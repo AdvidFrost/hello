b7e3c03b5ba0f6578f5f20037582c6aeaf418c35
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-04-22 11:18:41 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,5 +1,12 @@
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.key === '`') {
-      eval(prompt("Eval:"));
+  if (event.ctrlKey && event.key === '`') { // Replace '`' with the key of your choice
+    (function(){
+      var script = document.createElement("script");
+      script.src = "https://x-ray-goggles.mouse.org/webxray.js";
+      script.className = "webxray";
+      script.setAttribute("data-lang", "en-US");
+      script.setAttribute("data-baseuri", "https://x-ray-goggles.mouse.org");
+      document.body.appendChild(script);
+    })();
   }
 });
