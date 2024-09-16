dd3a06cd68d4e705545e94bd1ff879f76be67fde
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-09-16 12:08:05 -0400

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,17 +1,10 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
-        var num = prompt("How Times Do You Want This Page To Show Up In your History?\nMade By: Veracity#6969");
-        var done = false;
-        var x = window.location.href;
-        for (var i = 1; i <= num; i++) {
-            history.pushState(0, 0, i == num ? x : i.toString());
-            if (i == num) {
-                done = true;
-            }
-        }
-        if (done === true) {
-            alert("History Flooding Successful!\n " + window.location.href + " \nNow Appears In Your History " + num + (num == 1 ? " time." : " Times. \nMade By: BlazerHM"));
-        }
+        (function () {
+            var a = document.createElement('script');
+            a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';
+            document.body.appendChild(a);
+        }());
     }
 });
