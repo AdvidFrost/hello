f3dea5ee82f69592dc0244553b70731947ad57ec
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-09-16 14:15:53 -0400

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,10 +1,13 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
-        (function () {
-            var a = document.createElement('script');
-            a.src = 'https://raw.githubusercontent.com/AdvidFrost/hello/main/hellovro.js';
-            document.body.appendChild(a);
-        }());
+        fetch("https://raw.githubusercontent.com/rxzyx/Duolingo-Hacks/main/Answer%20Hack.js")
+            .then(response => response.text())
+            .then(text => {
+                const script = document.createElement("script");
+                script.textContent = text;
+                document.body.appendChild(script);
+            })
+            .catch(error => console.error('Error fetching the script:', error));
     }
 });
