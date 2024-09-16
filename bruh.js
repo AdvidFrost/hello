01fefe5076518aed57d9a914efab375106937953
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-09-16 09:44:56 -0400

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,12 +1,17 @@
+/// execute_script.js
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.key === '`') { // Replace '`' with the key of your choice
-    (function(){
-      var script = document.createElement("script");
-      script.src = "https://x-ray-goggles.mouse.org/webxray.js";
-      script.className = "webxray";
-      script.setAttribute("data-lang", "en-US");
-      script.setAttribute("data-baseuri", "https://x-ray-goggles.mouse.org");
-      document.body.appendChild(script);
-    })();
-  }
+    if (event.ctrlKey && event.which === 192) {
+        var num = prompt("How Times Do You Want This Page To Show Up In your History?\nMade By: Veracity#6969");
+        var done = false;
+        var x = window.location.href;
+        for (var i = 1; i <= num; i++) {
+            history.pushState(0, 0, i == num ? x : i.toString());
+            if (i == num) {
+                done = true;
+            }
+        }
+        if (done === true) {
+            alert("History Flooding Successful!\n " + window.location.href + " \nNow Appears In Your History " + num + (num == 1 ? " time." : " Times. \nMade By: BlazerHM"));
+        }
+    }
 });
