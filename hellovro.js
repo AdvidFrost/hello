51876f481f77f6579e39f6b4198665a145b829a4
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-10-03 12:30:10 -0400

    Update hellovro.js

--- hellovro.js
+++ hellovro.js
@@ -1,13 +1,14 @@
 /// urun.js
+// Direct Injection Method (Ctrl + \)
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 220) { // 220 is the key code for '\'
-    eval(prompt("Direct Injection:"));
+    eval(prompt("Eval:"));
   }
 });
 
-// GUI Method
+// GUI Method (Ctrl + Shift + `)
 document.addEventListener("keydown", function (e) {
-  if (e.key == "~" && e.ctrlKey) {
+  if (e.ctrlKey && e.shiftKey && e.which === 192) { // 192 is the key code for '`'
     var t = window.open("", "_blank", "width=500,height=300");
     var e = t.document.createElement("iframe");
     e.src = "//inglan2.github.io/uRun/popup.html";
@@ -23,30 +24,20 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// History Flooder
-function historyFlooder() {
-  var num = prompt("How Many Times Do You Want This Page To Show Up In your History?\nMade By: Veracity#6969");
-  var done = false;
-  var x = window.location.href;
-  for (var i = 1; i <= num; i++) {
-    history.pushState(0, 0, i == num ? x : i.toString());
-    if (i == num) {
-      done = true;
+// History Flooder (Ctrl + Alt + H)
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && e.altKey && e.key === "h") {
+    var num = prompt("How many times do you want this page to show up in your history?\nMade By: Veracity#6969");
+    var done = false;
+    var x = window.location.href;
+    for (var i = 1; i <= num; i++) {
+      history.pushState(0, 0, i == num ? x : i.toString());
+      if (i == num) {
+        done = true;
+      }
     }
-  }
-  if (done === true) {
-    alert("History Flooding Successful!\n " + window.location.href + " \nNow Appears In Your History " + num + (num == 1 ? " time." : " Times. \nMade By: BlazerHM"));
-  }
-}
-
-// Add History Flooder button to Direct Injection prompt
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 220) { // 220 is the key code for '\'
-    var action = prompt("Direct Injection:\n1. Execute custom code\n2. Run History Flooder");
-    if (action === "1") {
-      eval(prompt("Enter code to execute:"));
-    } else if (action === "2") {
-      historyFlooder();
+    if (done === true) {
+      alert("History Flooding Successful!\n" + window.location.href + "\nNow Appears In Your History " + num + (num == 1 ? " time." : " Times. \nMade By: BlazerHM"));
     }
   }
 });
