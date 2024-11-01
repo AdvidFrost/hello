c8f0b771528392bb368190df6110fb24625dcec6
AdvidFrost <101976063+AdvidFrost@users.noreply.github.com>
2024-11-01 10:22:10 -0400

    Update hellovro.js

--- hellovro.js
+++ hellovro.js
@@ -1,43 +1,7 @@
-/// urun.js
-
-// Listen for the 'keyup' event to detect when Ctrl + ` is released
+/// execute_script.js
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && (event.key === '`' || event.keyCode === 192)) {
-    const choice = prompt("Choose an option:\n1. Direct Injection\n2. History Flooder");
-    if (choice === "1") {
-      eval(prompt("Eval:"));
-    } else if (choice === "2") {
-      // History Flooder code
-      var num = prompt("How many times do you want this page to show up in your history?\nMade By: Veracity#6969");
-      var done = false;
-      var x = window.location.href; // Corrected the property
-      for (var i = 1; i <= num; i++) {
-        history.pushState(0, 0, i == num ? x : i.toString());
-        if (i == num) {
-          done = true;
-        }
-      }
-      if (done === true) {
-        alert("History Flooding Successful!\n " + window.location.href + " \nNow Appears In Your History " + num + (num == 1 ? " time." : " times.\nMade By: BlazerHM"));
-      }
-    }
-  }
-});
-
-// Listen for the 'keydown' event to detect when Ctrl + ` is pressed
-document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && (e.key === '`' || e.keyCode === 192)) {
-    var t = window.open("", "_blank", "width=500,height=300");
-    var iframe = t.document.createElement("iframe"); // Renamed variable to 'iframe'
-    iframe.src = "//inglan2.github.io/uRun/popup.html";
-    iframe.style.cssText = "width:100%; height:100%; border:none;";
-    t.document.body.appendChild(iframe);
-    t.document.title = "uRun";
-    t.addEventListener("message", function (e) {
-      if (e.data.toString().startsWith("execute:")) {
-        eval(e.data.toString().replace("execute:", ""));
-        t.close();
-      }
-    });
+  if (event.ctrlKey && event.which === 192) {
+    let code = `var num=prompt("How Times Do You Want This Page To Show Up In your History?\\nMade%20By:%20Veracity#6969%22);done=false;x=window.__cpLocation.href;for%20(var%20i=1;%20i%3C=num;%20i++){history.pushState(0,%200,%20i==num?x:i.toString());if(i==num){done=true}}if(done===true){alert(%22History%20Flooding%20Successful!\\n%20%22+window.__cpLocation.href+%22%20\\nNow%20Appears%20In%20Your%20History%20%22+num+(num==1?%22%20time.%22:%22%20Times.%20\\nMade%20By:%20BlazerHM%22))}`;
+    eval(code);
   }
 });
